6c52dcf85dde9aaaa176e2056fcf09b58f58896f
Joshua Epstein <18082@jfs.brent.sch.uk>
2024-09-21 17:29:26 +0100

    Remove unused UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-// import DeckIconDisplay from '@/components/deck-icon-display'
 import DeckRenderer from '@/components/deck-renderer'
-// import ItemDisplay from '@/components/item-display'
 import SelectDeck from '@/components/SelectDeck'
 import MaxWidthWrapper from '@/components/shared/max-width-wrapper'
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '@/components/ui/dialog'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
-import decksConfig from '@/config/decks.json'
 import { getDecks, getTexture, isDeck } from '@/types/deck'
-import * as TooltipPrimitive from '@radix-ui/react-tooltip'
-import Image from 'next/image'
-import Link from 'next/link'
 import { notFound, redirect } from 'next/navigation'
-import treasureDeck from '~/deck/treasure.png'
 import { RedirectWithCards } from './redirectWithCards'
 
 export default async function HomePage({ searchParams: { selectedDeck, cards } }) {
