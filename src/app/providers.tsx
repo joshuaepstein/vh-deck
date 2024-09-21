d0937bb8a84f702fcfbf34a8602905e0dbd9946b
Joshua Epstein <18082@jfs.brent.sch.uk>
2024-09-21 17:30:27 +0100

    chore: Remove unused TooltipProvider from Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,3 @@
-import { TooltipProvider } from '@/components/ui/tooltip'
-
 export default function Providers({ children, ...props }: { children: React.ReactNode }) {
-    return <TooltipProvider>{children}</TooltipProvider>
+    return children
 }
